bf73fa7ecc2776094f750d09c3f48db11aa6f7af
SangDon <tkdehs0454@naver.com>
2022-05-25 17:25:52 +0900

    flatMap 테스트중
    flatMap 테스트중

--- app/src/main/java/com/pnx/MainActivity.kt
+++ app/src/main/java/com/pnx/MainActivity.kt
@@ -9,11 +9,16 @@ import com.jakewharton.rxbinding4.view.clicks
 import com.jakewharton.rxbinding4.widget.textChanges
 import com.pnx.databinding.ActivityMainBinding
 import io.reactivex.Observable
+import io.reactivex.ObservableSource
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.functions.BiFunction
 import io.reactivex.rxjava3.kotlin.subscribeBy
 import io.reactivex.rxjava3.kotlin.toObservable
 import io.reactivex.rxjava3.schedulers.Schedulers
+import io.reactivex.subjects.PublishSubject
+import java.util.concurrent.TimeUnit
+import io.reactivex.functions.Function
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,12 +29,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val mapper1 = Function<String, Observable<String>> {
+            Observable.just("$it A", "$it B")
+        }
+
+
         binding.btn1.clicks()
             .subscribeOn(AndroidSchedulers.mainThread())
             .subscribe {
                 val list = arrayListOf("1","2","3","4","5","6","7","8")
-                val observarble = list.toObservable()
-                observarble.subscribeBy(
+                list.toObservable()
+                    .subscribeBy(
                     onNext = {
                         Log.d("RX","onNext : $it")
                     },
@@ -40,6 +50,22 @@ class MainActivity : AppCompatActivity() {
                         Log.d("RX","onError : $it")
                     }
                 )
+            }
+        val subject = PublishSubject.create<String>()
+
+        subject.onNext("")
+        subject.subscribe {
+            // 구독 이벤트
+        }
+
+        val mapper = Function<Int, Observable<String>> {
+            Observable.just("$it A", "$it B")
+        }
+
+        Observable.fromArray(1, 2, 3)
+            .flatMap(mapper)
+            .subscribe {
+                Log.d("RX","$it")
             }
     }
 }
